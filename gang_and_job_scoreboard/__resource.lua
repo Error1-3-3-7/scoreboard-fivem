713c7a875a87694c5c9f7ec7b9fec583d8225f6d
uid <104393518+Error1-3-3-7@users.noreply.github.com>
2022-11-26 21:10:37 +0200

    Add files via upload

---
+++ gang_and_job_scoreboard/__resource.lua
@@ -0,0 +1,19 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+description 'gang_scoreboard'
+
+version '1.0.0'
+
+author 'uid#8612'
+
+server_script 'server/main.lua'
+
+client_script 'client/main.lua'
+
+ui_page 'html/scoreboard.html'
+
+files {
+	'html/scoreboard.html',
+	'html/style.css',
+	'html/listener.js'
+}
